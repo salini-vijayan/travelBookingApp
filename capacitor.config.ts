90287366e8e76e8e9219b4bc8f2896e2808aff30
Salini Vijayan <amsalinivijayan@gmail.com>
2021-12-03 10:49:26 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'travelBookingApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
